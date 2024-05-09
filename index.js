079157a5188e1f34a29f8eb313f4178d9638ed93
atengelsgjerd <atengelsgjerd@gmail.com>
2024-05-09 16:28:25 -0500

    Added color array and color validation function

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
-const colors = require("colors");
 const fs = require("fs");
-const generateSVG = require("./lib/shapes.js");
+const {Circle, Square, Triangle, generateSVG} = require("./lib/shapes.js");
 
+const colors = ['aliceblue', 'antiquewhite', 'aqua', 'aquaMarine', 'azure', 'beige', 'bisque', 'black', 'blanchedalmond', 'blue', 'blueviolet', 'brown', 'burlywood', 'cadetblue', 'chartreuse', 'chocolate', 'coral', 'cornflowerblue', 'cornsilk', 'crimson', 'cyan', 'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgray', 'darkgrey', 'darkgreen', 'darkkhaki', 'darkmagenta', 'darkolivegreen', 'darkorange', 'darkorchid', 'darkred', 'darksalmon', 'darkseagreen', 'darkslateblue', 'darkslategray', 'darkslategrey', 'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue', 'dimgray', 'dimgrey', 'dodgerblue', 'firebrick', 'floralwhite', 'forestgreen', 'fuchsia', 'gainsboro', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'grey', 'green', 'greenyellow', 'honeydew', 'hotpink', 'indianred', 'indigo', 'ivory', 'khaki', 'lavender', 'lavenderblush', 'lawngreen', 'lemonchiffon', 'lightblue', 'lightcoral', 'lightcyan', 'lightgoldenrodyellow', 'lightgray', 'lightgrey', 'lightgreen', 'lightpink', 'lightsalmon', 'lightseagreen', 'lightskyblue', 'lightslategray', 'lightslategrey', 'lightsteelblue', 'lightyellow', 'lime', 'limegreen', 'linen', 'magenta', 'maroon', 'mediumaquamarine', 'mediumblue', 'mediumorchid', 'mediumpurple', 'mediumseagreen', 'mediumslateblue', 'mediumspringgreen', 'mediumturquoise', 'mediumvioletred', 'midnightblue', 'mintcream', 'mistyrose', 'moccasin', 'navajowhite', 'navy', 'oldlace', 'olive', 'olivedrab', 'orange', 'orangered', 'orchid', 'palegoldenrod', 'palegreen', 'paleturquoise', 'palevioletred', 'papayawhip', 'peachpuff', 'peru', 'pink', 'plum', 'powderblue', 'purple', 'rebeccapurple', 'red', 'rosybrown', 'royalblue', 'saddlebrown', 'salmon', 'sandybrown', 'seagreen', 'seashell', 'sienna', 'silver', 'skyblue', 'slateblue', 'slategray', 'slategrey', 'snow', 'springgreen', 'steelblue', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'wheat', 'white', 'whitesmoke', 'yellow', 'yellowgreen'];
 
+function colorValidation(answer) {
+    if (colors.includes(answer)) {
+        return true;
+    }
+    return "Please enter a valid color";
+}
+
+// console.log(colorKeywords.length);
 
 // const writeFileAsync = util.promisify(fs.writeFile);
 // TODO: Create an array of questions for user input
@@ -14,8 +22,8 @@ const questions = [
         message: "Please enter up to 3 characters",
         name: "characters",
         validate: (answer) => {
-            if (answer.length > 3) {
-                return console.log("\n Please enter only up to 3 characters");
+            if (answer.length === 0 | answer.length > 3) {
+                return console.log("\n Please enter 1 to 3 characters");
             }
             return true;
         }
@@ -23,12 +31,18 @@ const questions = [
     {
         type: "input",
         message: "Please enter a color for the characters",
-        name: "characterColor"
+        name: "characterColor",
+        validate: (answer) => {
+            return colorValidation(answer);
+        }
     },
     {
         type: "input",
         message: "Please enter a color for the shape",
-        name: "shapeColor"
+        name: "shapeColor",
+        validate: (answer) => {
+            return colorValidation(answer);
+        }
     },
     {
         type: "list",
@@ -47,5 +61,6 @@ inquirer.prompt(questions).then(function(answers){
 }
 
 init();
+
 
 
